0b4f481487a782c1132f04c5612829d2bdfd43f1
jawad <m_jawad_butt@yahoo.com>
2025-08-11 18:47:07 +1000

    Move client to top-level main dir.

--- src/main/client/next.config.ts
+++ src/main/client/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: '../resources/static/client', // replaces `.next` with `build`
 };
 
 export default nextConfig;
